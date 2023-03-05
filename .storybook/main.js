0af757ab4e667359eb1ee99aebe8e5f72d00fadd
Owen Buckley <owen@thegreenhouse.io>
2023-03-05 10:18:36 -0500

    upgrade postcss 8

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,14 @@
 const config = {
   staticDirs: ['../src'],
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-postcss'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', {
+    name: '@storybook/addon-postcss',
+    options: {
+      postcssLoaderOptions: {
+        implementation: require('postcss'),
+      },
+    },
+  }],
   framework: {
     name: '@storybook/web-components-webpack5',
     options: {}

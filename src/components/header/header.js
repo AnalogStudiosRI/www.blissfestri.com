309af8b87ddb8e82ebf9c932c81d865fcc6e307d
Owen Buckley <owen@thegreenhouse.io>
2023-03-05 20:37:18 -0500

    acquired custom fonts

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,7 +2,7 @@ export default class Header extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
       <header style="background-color: var(--color-primary)">
-        <h1 style="color: var(--color-secondary)">Welcome to Blissfest!</h1>
+        <h1 style="color: var(--color-secondary); font-family: var(--font-secondary)">Welcome to Blissfest!</h1>
       </header>
     `;
   }

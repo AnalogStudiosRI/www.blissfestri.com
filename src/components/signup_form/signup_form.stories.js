c38389700a4b2ba6c8b25223756c98c18e3daf5b
aholtzman <adam.holtzman@gmail.com>
2023-03-12 12:57:25 -0400

    initial struture of signup form component

---
+++ src/components/signup_form/signup_form.stories.js
@@ -0,0 +1,11 @@
+import '../../styles/theme.css';
+import './index.js';
+
+export default {
+  title: 'Components/Signup Form'
+};
+
+// More on component templates: https://storybook.js.org/docs/web-components/writing-stories/introduction#using-args
+const Template = () => '<bf-signup_form></bf-signup_form>';
+
+export const Primary = Template.bind({});

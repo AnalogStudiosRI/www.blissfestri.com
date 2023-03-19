8a7d909e4b6a2dbe29fee041fc51b23d6cbc9cce
aholtzman <adam.holtzman@gmail.com>
2023-03-19 08:39:24 -0400

    add a11y color, tests, border, polish

--- src/components/signup_form/index.js
+++ src/components/signup_form/index.js
@@ -1,15 +1,15 @@
 export default class SignupForm extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
-      <div class="bg-primary p-6 text-center" style="max-width:300px;">
-        <img src="" alt="popped open bottle with music sheet" />
+      <div class="bg-primary p-3 text-center m-4" id="signup_form">
+        <img src="assets/images/bottle.png" alt="popped open bottle with music sheet" />
         <h2
           class="text-secondary text-3xl font-primary text-secondary uppercase mt-4 mb-4 w-2/3 m-auto"
           >
             Stay in Touch!
           </h2>
         <p
-          class="text-white mb-6"
+          class="text-white mb-6 w-3/4 m-auto"
         >
             Subscribe to stay up to date on this summer's upcomming music festivities!
         </p>
@@ -38,13 +38,13 @@ export default class SignupForm extends HTMLElement {
           </label>
           <button
             type="submit"
-            class="bg-secondary p-4 rounded-3xl text-black font-bold w-2/3 m-auto mt-4"
+            class="bg-secondary p-2 rounded-3xl text-primary font-bold w-1/2 m-auto mt-4 font-primary"
           >
             Sign Me Up
           </button>
         </form>
         <p class="mt-8">Social bar TK</p>
-        <small class="text-white">©Analog Studios, All Rights Reserved.</small>
+        <small class="text-white">©Analog Studios. All Rights Reserved.</small>
       </div>
     `;
   }
--- src/components/signup_form/signup_form.spec.js
+++ src/components/signup_form/signup_form.spec.js
@@ -1,32 +1,47 @@
 import { expect } from '@esm-bundle/chai';
-import './header.js';
+import './index.js';
 
-describe('Components/Header', () => {
-  let header;
+describe('Components/Signup Form', () => {
+  let form;
 
   before(async () => {
-    header = document.createElement('bf-header');
-    document.body.appendChild(header);
+    form = document.createElement('bf-signup_form');
+    document.body.appendChild(form);
 
-    await header.updateComplete;
+    await form.updateComplete;
   });
 
   describe('Default Behavior', () => {
     it('should not be null', () => {
-      expect(header).not.equal(undefined);
-      expect(header.querySelectorAll('header').length).equal(1);
+      expect(form).not.equal(undefined);
+      expect(form.querySelectorAll('form').length).equal(1);
     });
 
-    it('should have the expected heading text', () => {
-      const heading = document.getElementsByTagName('h1')[0];
+    it('should have the expected text', () => {
+      const image = document.getElementsByTagName('img')[0];
+      const heading = document.getElementsByTagName('h2')[0];
+      const tagLine = document.getElementsByTagName('p')[0];
+      const button = document.getElementsByTagName('button')[0];
+      const copyright = document.getElementsByTagName('small')[0];
+      const lables = document.getElementsByTagName('label');
 
-      expect(heading.textContent).to.equal('Welcome to Blissfest!');
+      expect(image.alt).to.equal('popped open bottle with music sheet');
+      expect(heading.textContent.trim()).to.equal('Stay in Touch!');
+      expect(tagLine.textContent.trim()).to.equal(
+        'Subscribe to stay up to date on this summer\'s upcomming music festivities!'
+      );
+      expect(lables[0].textContent.trim()).to.equal('Name (optional)');
+      expect(lables[1].textContent.trim()).to.equal('Email Address');
+      expect(button.textContent.trim()).to.equal('Sign Me Up');
+      expect(copyright.textContent).to.equal(
+        '©Analog Studios. All Rights Reserved.'
+      );
     });
   });
 
   after(() => {
-    header.remove();
-    header = null;
+    form.remove();
+    form = null;
   });
 
 });

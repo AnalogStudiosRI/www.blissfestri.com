c439685a603c73f400ddb79786cbd9380c2b0b03
Owen Buckley <owen@thegreenhouse.io>
2023-03-25 15:42:47 -0400

    CSS refactoring and basic responsiveness

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,15 +2,15 @@ export default class Header extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
       <header
-        class="pb-20"
-        style="background-image: url('/assets/images/tear-strip.webp'); background-repeat: repeat-x; background-position: bottom"
+        class="pb-20 bg-repeat-x bg-bottom"
+        style="background-image: url('/assets/images/tear-strip.webp');"
       >
         <div class="text-center bg-primary">
           <img
-            class="inline-block w-1/2 pt-10 pb-10"
+            class="inline-block w-1/2 pt-6 pb-5 md:pt-10 md:pb-10"
             src="/assets/images/blissfest-logo.webp"
             alt="Blissfest logo"
-          >
+          />
         </div>
       </header>
     `;

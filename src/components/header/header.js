02fad6c70a8deb31359098046f0280bf4dfdf7c5
Owen Buckley <owen@thegreenhouse.io>
2023-03-25 15:01:45 -0400

    add Blissfest logo image and show in header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,8 +1,14 @@
 export default class Header extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
-      <header class="bg-primary">
-        <h1 class="text-secondary font-secondary text-center">Welcome to Blissfest!</h1>
+      <header
+        class="bg-primary text-center"
+      >
+        <img
+          class="inline-block w-1/2 p-8"
+          src="/assets/images/blissfest-logo.png"
+          alt="Blissfest logo"
+        >
       </header>
     `;
   }

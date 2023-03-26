d0c6c3ba8be7c601bf6f01cc63b8b16d48411da3
Owen Buckley <owen@thegreenhouse.io>
2023-03-25 20:15:02 -0400

    add logos and locations

--- src/components/event-details/event-details.js
+++ src/components/event-details/event-details.js
@@ -2,12 +2,12 @@ export default class EventDetails extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
       <div
-        class="bg-no-repeat h-60 font-secondary"
+        class="bg-no-repeat h-60"
         style="height: 900px; width: 900px; background-image:url('/assets/images/ri-map.webp'); padding-left: min(460px);"
       >
 
         <h1
-          class="text-accent text-6xl text-center"
+          class="text-accent text-6xl text-center font-secondary"
         >
           <span class="flex flex-nowrap justify-between pt-4 pb-4">
             <span>R</span>
@@ -50,14 +50,38 @@ export default class EventDetails extends HTMLElement {
           </span>
         </h1>
 
-        <h2 class="text-primary text-6xl pb-2 pl-44">
-          <span flex-flex-nowrap>7 &dot; 29 &dot; 2023</span>
+        <h2 class="text-primary text-6xl pb-2 pl-44 font-secondary">
+          <span>7 &dot; 29 &dot; 2023</span>
         </h2>
 
-        <h2 class="text-accent text-4xl m-2 pl-44">
+        <h2 class="text-accent text-4xl m-2 pl-44 font-secondary">
           <span>5:00pm to 11:00pm<span>
         </h2>
 
+        <div class="pl-32 pt-40">
+          <div class="flex">
+            <div class="w-2/5">
+              <img src="/assets/images/iyrs-logo.webp" alt="Iyrs logo">
+            </div>
+            <div class="pl-4">
+              <h3 class="text-accent text-2xl font-secondary">Restoration Hall</h3>
+              <h4 class="text-primary italic text-xl">449 Thames Street</h4>
+              <h4 class="text-primary italic text-xl">Newport&#44; RI 02840</h4>
+            </div>
+          </div>
+
+          <div class="flex pt-8">
+            <div class="w-2/5">
+              <img src="/assets/images/analog-studios-logo.webp" alt="Analog Studios logo">
+            </div>
+            <div class="pl-4">
+              <h4 class="text-primary pt-4 text-xl">Presented by</h4>
+              <h4 class="text-primary text-xl underline">
+                <a href="https://www.analogstudios.net" title="Analog Studios website">Analog Studios</a>
+              </h4>
+            </div>
+          </div>
+        </div>
       </div>
     `;
   }

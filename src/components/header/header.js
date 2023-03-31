2f83a1f5b2b5d000d3cd9bea628b431a6fdb6e2a
Owen Buckley <owen@thegreenhouse.io>
2023-03-30 21:52:37 -0400

    Feature/issue 5 header component (#25)
    * add Blissfest logo image and show in header
    
    * add tearing aesthetic to bottom of header
    
    * update header spec file
    
    * CSS refactoring and basic responsiveness
    
    * adjust logo size across breakpoints

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,8 +1,17 @@
 export default class Header extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
-      <header class="bg-primary">
-        <h1 class="text-secondary font-secondary text-center">Welcome to Blissfest!</h1>
+      <header
+        class="pb-20 bg-repeat-x bg-bottom"
+        style="background-image: url('/assets/images/tear-strip.webp');"
+      >
+        <div class="text-center bg-primary">
+          <img
+            class="inline-block w-1/2 lg:w-1/3 pt-5 pb-5 md:pt-10 md:pb-10"
+            src="/assets/images/blissfest-logo.webp"
+            alt="Blissfest logo"
+          />
+        </div>
       </header>
     `;
   }

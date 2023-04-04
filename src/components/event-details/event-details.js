a03aa68c8616f9d43ac3e7414295b4f9e65a423b
Owen Buckley <owen@thegreenhouse.io>
2023-04-03 21:58:11 -0400

    mobile refactoring for event details component

--- src/components/event-details/event-details.js
+++ src/components/event-details/event-details.js
@@ -3,13 +3,12 @@ export default class EventDetails extends HTMLElement {
     this.innerHTML = `
       <div
         class="bg-no-repeat h-60 event-details"
-        style="height: 900px; width: 900px; background-image:url('/assets/images/ri-map.webp'); padding-left: min(460px);"
       >
-
+        <img src="/assets/images/ri-map.webp" class="inline" style="max-width: 50%; position:absolute">
         <h1
-          class="text-accent text-6xl text-center font-secondary"
+          class="text-accent font-secondary text-2xl pl-40l"
         >
-          <span class="flex flex-nowrap justify-between pt-4 pb-4">
+          <span class="flex flex-nowrap justify-between">
             <span>R</span>
             <span>h</span>
             <span>o</span>
@@ -25,7 +24,7 @@ export default class EventDetails extends HTMLElement {
             <span>&apos;</span>
             <span>s</span>
           </span>
-          <span class="flex flex-nowrap justify-between text-8xl">
+          <span class="flex flex-nowrap justify-between">
             <span>I</span>
             <span>n</span>
             <span>d</span>
@@ -38,7 +37,7 @@ export default class EventDetails extends HTMLElement {
             <span>i</span>
             <span>c</span>
           </span>
-          <span class="flex flex-nowrap pl-72 w-60 justify-evenly text-8xl pt-4 pb-4">
+          <span class="flex flex-nowrap justify-evenly pl-12 text-4xl">
             <span>F</span>
             <span>e</span>
             <span>s</span>
@@ -50,33 +49,33 @@ export default class EventDetails extends HTMLElement {
           </span>
         </h1>
 
-        <h2 class="text-primary text-6xl pb-2 pl-44 font-secondary">
+        <h2 class="text-primary font-secondary text-2xl pb-2 pl-60 ml-2">
           <span>7 &dot; 29 &dot; 2023</span>
         </h2>
 
-        <h2 class="text-accent text-4xl m-2 pl-44 font-secondary">
+        <h2 class="text-accent font-secondary text-2xl pl-52 ml-2">
           <span>5:00pm to 11:00pm<span>
         </h2>
 
-        <div class="pl-32 pt-40">
+        <div class="pl-44 pt-4 mt-1">
           <div class="flex location">
-            <div class="w-2/5">
+            <div class="w-1/3">
               <img src="/assets/images/iyrs-logo.webp" alt="Iyrs logo">
             </div>
             <div class="pl-4 content">
-              <h3 class="text-accent text-2xl font-secondary">Restoration Hall</h3>
-              <h4 class="text-primary italic text-xl">449 Thames Street</h4>
-              <h4 class="text-primary italic text-xl">Newport&#44; RI 02840</h4>
+              <h3 class="text-accent lg:text-2xl font-secondary">Restoration Hall</h3>
+              <h4 class="text-primary italic text-md">449 Thames Street</h4>
+              <h4 class="text-primary italic text-md">Newport&#44; RI</h4>
             </div>
           </div>
 
-          <div class="flex pt-8 presenter">
-            <div class="w-2/5">
+          <div class="flex pt-4 presenter">
+            <div class="w-1/3">
               <img src="/assets/images/analog-studios-logo.webp" alt="Analog Studios logo">
             </div>
             <div class="pl-4 content">
-              <h3 class="text-primary pt-4 text-xl">Presented by</h3>
-              <h4 class="text-primary text-xl underline">
+              <h3 class="text-primary text-xl">Presented by</h3>
+              <h4 class="text-accent font-secondary text-xl underline">
                 <a href="https://www.analogstudios.net" title="Analog Studios website">Analog Studios</a>
               </h4>
             </div>

54a82e62e128d60afd2e34469d282d5c457fc203
Owen Buckley <owen@thegreenhouse.io>
2023-04-04 09:05:02 -0400

    further refining event details component for smallest breakpoints

--- src/components/event-details/event-details.js
+++ src/components/event-details/event-details.js
@@ -6,7 +6,7 @@ export default class EventDetails extends HTMLElement {
       >
         <img src="/assets/images/ri-map.webp" class="inline" style="max-width: 50%; position:absolute">
         <h1
-          class="text-accent font-secondary text-2xl pl-40l"
+          class="text-accent font-secondary text-2xl pl-36 ml-1"
         >
           <span class="flex flex-nowrap justify-between">
             <span>R</span>
@@ -37,7 +37,7 @@ export default class EventDetails extends HTMLElement {
             <span>i</span>
             <span>c</span>
           </span>
-          <span class="flex flex-nowrap justify-evenly pl-12 text-4xl">
+          <span class="flex flex-nowrap justify-evenly pl-8 text-3xl ml-1">
             <span>F</span>
             <span>e</span>
             <span>s</span>
@@ -49,33 +49,33 @@ export default class EventDetails extends HTMLElement {
           </span>
         </h1>
 
-        <h2 class="text-primary font-secondary text-2xl pb-2 pl-60 ml-2">
+        <h2 class="text-primary font-secondary text-2xl pl-52 ml-2">
           <span>7 &dot; 29 &dot; 2023</span>
         </h2>
 
-        <h2 class="text-accent font-secondary text-2xl pl-52 ml-2">
+        <h2 class="text-accent font-secondary text-md pl-52 ml-1">
           <span>5:00pm to 11:00pm<span>
         </h2>
 
-        <div class="pl-44 pt-4 mt-1">
+        <div class="pl-40 pt-8 mt-1">
           <div class="flex location">
-            <div class="w-1/3">
+            <div class="w-1/4 mr-2">
               <img src="/assets/images/iyrs-logo.webp" alt="Iyrs logo">
             </div>
-            <div class="pl-4 content">
-              <h3 class="text-accent lg:text-2xl font-secondary">Restoration Hall</h3>
-              <h4 class="text-primary italic text-md">449 Thames Street</h4>
-              <h4 class="text-primary italic text-md">Newport&#44; RI</h4>
+            <div class="content">
+              <h3 class="text-accent font-secondary">Restoration Hall</h3>
+              <h4 class="text-primary italic text-sm">449 Thames Street</h4>
+              <h4 class="text-primary italic text-sm">Newport&#44; RI</h4>
             </div>
           </div>
 
           <div class="flex pt-4 presenter">
-            <div class="w-1/3">
+            <div class="w-1/4 mr-2">
               <img src="/assets/images/analog-studios-logo.webp" alt="Analog Studios logo">
             </div>
-            <div class="pl-4 content">
-              <h3 class="text-primary text-xl">Presented by</h3>
-              <h4 class="text-accent font-secondary text-xl underline">
+            <div class="content">
+              <h3 class="text-primary text-lg">Presented by</h3>
+              <h4 class="text-accent font-secondary text-sm underline">
                 <a href="https://www.analogstudios.net" title="Analog Studios website">Analog Studios</a>
               </h4>
             </div>

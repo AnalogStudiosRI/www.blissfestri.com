86305814a3a4e36aa241a096620f669e50f6e833
Owen Buckley <owen@thegreenhouse.io>
2023-04-13 09:06:14 -0400

    consistent responsiveness for header and signup form

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,12 +2,12 @@ export default class Header extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
       <header
-        class="pb-20 bg-repeat-x bg-bottom"
+        class="pb-16 bg-repeat-x bg-bottom"
         style="background-image: url('/assets/images/tear-strip.webp');"
       >
         <div class="text-center bg-primary">
           <img
-            class="inline-block w-1/2 lg:w-1/3 pt-5 pb-5 md:pt-10 md:pb-10"
+            class="inline-block w-2/3 pt-5 pb-5 lg:w-1/2"
             src="/assets/images/blissfest-logo.webp"
             alt="Blissfest logo"
           />
--- src/components/signup-form/signup-form.js
+++ src/components/signup-form/signup-form.js
@@ -3,53 +3,56 @@ export default class SignupForm extends HTMLElement {
     this.innerHTML = `
       <div
         id="signup-form"
-        class="bg-primary p-1 text-center m-4 max-w-xs">
+        class="font-primary bg-primary text-primary text-center p-1 m-4"
+      >
         <img
           src="/assets/images/message-bottle.png"
           alt="popped open bottle with music sheet"
           class="inline-block w-32"
           loading="lazy"
         />
-        <h2
-          class="text-secondary text-4xl font-primary font-bold uppercase mt-4 mb-4"
+        <div class="mr-auto ml-auto md:w-3/5 xl:w-2/5">
+          <h2
+            class="font-bold text-secondary text-4xl uppercase mt-4 mb-4"
+            >
+              Stay in Touch!
+            </h2>
+          <p
+            class="text-white mb-6 m-auto"
           >
-            Stay in Touch!
-          </h2>
-        <p
-          class="text-white mb-6 m-auto"
-        >
-            Subscribe to stay up to date for this summer's upcoming music festivities!
-        </p>
-        <form
-          name="sign-up"
-          method="POST"
-          data-netlify="true"
-          class="width-full flex flex-col gap-4"
-        >
-          <label class="text-white text-left w-full">
-            Name (optional)
-            <input
-              type="text"
-              class="w-full p-2 rounded-lg text-black"
-              name="name"
-            />
-          </label>
-          <label class="text-white text-left w-full">
-            Email Address
-            <input
-              type="email"
-              name="email"
-              class="w-full p-2 rounded-lg text-black"
-              required
-            />
-          </label>
-          <button
-            type="submit"
-            class="bg-secondary p-2 rounded-3xl text-primary font-bold w-1/2 m-auto mt-4 font-primary"
+              Subscribe to stay up to date for this summer's upcoming music festivities!
+          </p>
+          <form
+            name="sign-up"
+            method="POST"
+            data-netlify="true"
+            class="width-full flex flex-col gap-4"
           >
-            Sign Me Up
-          </button>
-        </form>
+            <label class="text-white text-left w-full">
+              Name (optional)
+              <input
+                type="text"
+                class="w-full p-2 rounded-lg text-black"
+                name="name"
+              />
+            </label>
+            <label class="text-white text-left w-full">
+              Email Address
+              <input
+                type="email"
+                name="email"
+                class="w-full p-2 rounded-lg text-black"
+                required
+              />
+            </label>
+            <button
+              type="submit"
+              class="bg-secondary p-2 rounded-3xl text-primary font-bold m-auto mt-4"
+            >
+              Sign Me Up
+            </button>
+          </form>
+        </div>
         <div class="flex gap-x-4 justify-center mb-1 mt-20">
           <a
             href="https://www.facebook.com/analogstudiosri/"

1b7477c64ecf9db9b522dbfa436604117e80e763
Owen Buckley <owen@thegreenhouse.io>
2023-05-04 21:03:51 -0400

    create navigation component

---
+++ src/components/navigation/navigation.js
@@ -0,0 +1,68 @@
+export default class Navigation extends HTMLElement {
+  constructor() {
+    super();
+    this.navLinks = [
+      {
+        name: 'Home',
+        url: '/'
+      }, {
+        name: 'About',
+        url: '/about/'
+      }, {
+        name: 'Lineup',
+        url: '/lineup/'
+      }, {
+        name: 'Tickets',
+        url: '/tickets/'
+      }, {
+        name: 'FAQ',
+        url: '/faq/'
+      }, {
+        name: 'Contact',
+        url: '/contact/'
+      }
+    ];
+  }
+
+  connectedCallback() {
+    const { navLinks } = this;
+
+    if (this.innerHTML !== '') {
+      // if already server-rendered, all we need to do is highlight the active link
+      Array.from(this.querySelectorAll('a')).forEach(link => {
+        const path = globalThis.location.pathname;
+        const isCurrentLink = link.getAttribute('href').replace(window.location.origin, '') === path;
+
+        link.style.color = isCurrentLink ? 'var(--color-tertiary)' : 'var(--color-white)';
+      });
+    } else {
+      this.innerHTML = `
+        <nav class="bg-primary font-secondary w-1/2 lg:w-full">
+          <ul class="text-white text-center">
+          ${
+            navLinks.map((link) => {
+              const { name, url } = link;
+              const path = typeof globalThis.location === 'undefined' ? null : globalThis.location.pathname;
+              const activePath = path === url;
+
+              return `
+                <li class="m-2 text-3xl inline-block w-1/4 lg:w-1/12 text-center">
+                  <a
+                    class="nav-link ${activePath ? 'text-tertiary' : 'text-white'} hover:text-tertiary"
+                    href="${url}"
+                    title="Visit the ${name} page"
+                  >
+                  ${name}
+                  </a>
+                </li>
+              `;
+            }).join('')
+          }
+          </ul>
+        </nav>
+      `;
+    }
+  }
+}
+
+customElements.define('bf-navigation', Navigation);
---
+++ src/components/navigation/navigation.spec.js
@@ -0,0 +1,65 @@
+import { expect } from '@esm-bundle/chai';
+import './navigation.js';
+
+describe('Components/Navigation', () => {
+  let nav;
+
+  before(async () => {
+    nav = document.createElement('bf-navigation');
+
+    document.body.appendChild(nav);
+
+    await nav.updateComplete;
+  });
+
+  describe('Default Behavior', () => {
+    it('should not be null', () => {
+      expect(nav).not.equal(undefined);
+      expect(nav.querySelectorAll('nav').length).equal(1);
+    });
+  });
+
+  describe('Nav Links', () => {
+    let links = [];
+
+    before(async () => {
+      links = nav.querySelectorAll('nav a.nav-link');
+    });
+
+    it('should have the expected number of links in the nav', () => {
+      expect(links.length).to.equal(6);
+    });
+
+    it('should have the expected links in the correct order', () => {
+      const navLinks = Array.from(links).map(link => link.getAttribute('href'));
+
+      expect(navLinks[0]).to.equal('/');
+      expect(navLinks[1]).to.equal('/about/');
+      expect(navLinks[2]).to.equal('/lineup/');
+      expect(navLinks[3]).to.equal('/tickets/');
+      expect(navLinks[4]).to.equal('/faq/');
+      expect(navLinks[5]).to.equal('/contact/');
+    });
+
+    it('should have the expected link labels in the correct order', () => {
+      const navText = Array.from(links).map(link => link.innerText.trim());
+
+      expect(navText[0]).to.equal('Home');
+      expect(navText[1]).to.equal('About');
+      expect(navText[2]).to.equal('Lineup');
+      expect(navText[3]).to.equal('Tickets');
+      expect(navText[4]).to.equal('FAQ');
+      expect(navText[5]).to.equal('Contact');
+    });
+
+    after(async () => {
+      links = [];
+    });
+  });
+
+  after(() => {
+    nav.remove();
+    nav = null;
+  });
+
+});

a36c25e15760f735176535572a427de792266c5a
Owen Buckley <owen@thegreenhouse.io>
2023-05-07 21:03:44 -0400

    rename signup form component to newsletter signup (#72)
    * rename signup form component to newsletter signup
    
    * netlify form name consistency
    
    * restore id for styling

---
+++ src/components/newsletter-signup/newsletter-signup.stories.js
@@ -0,0 +1,10 @@
+import '../../styles/theme.css';
+import './newsletter-signup.js';
+
+export default {
+  title: 'Components/Newsletter Signup'
+};
+
+const Template = () => '<bf-newsletter-signup></bf-newsletter-signup>';
+
+export const Primary = Template.bind({});

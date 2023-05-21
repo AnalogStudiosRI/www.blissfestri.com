d142b8c2708f24779dbf11efd7d8d6fca41402d7
Owen Buckley <owen@thegreenhouse.io>
2023-05-21 12:46:41 -0400

    handle lineup list layout across breakpoints

--- src/components/lineup/lineup.js
+++ src/components/lineup/lineup.js
@@ -5,21 +5,34 @@ export default class Lineup extends HTMLElement {
         class="p-6 font-primary w-full bg-cover bg-no-repeat bg-[url('/assets/images/iyrs-campus-ariel.webp')]"
       >
         <div
-          class="bg-white opacity-70"
+          class="bg-white opacity-70 pt-4"
         >
           <h2
             class="text-center text-5xl uppercase font-primary font-extrabold mb-8 scale-x-125 scale-y-150"
           >
             Lineup
           </h2>
-          <ul class="font-extrabold text-3xl uppercase">
-            <li class="text-center">Analog</li>
-            <li class="text-center">Bill Bartholomew</li>
+
+          <ul class="font-extrabold text-center text-3xl uppercase md:w-3/4 md:ml-auto md:mr-auto">
+            <li class="md:inline">Analog</li>
+            <li class="md:inline">
+              <img src="/assets/images/star.webp" alt="Artist name separator" width="3%" class="hidden m-4 md:inline"/>
+              Bill Bartholomew
+            </li>
           </ul>
 
-          <ul class="font-bold text-2xl uppercase">
-            <li class="text-center">Z-Boys</li>
-            <li class="text-center">Allison Cowrey</li>
+          <ul
+            class="font-bold text-center text-2xl uppercase md:w-2/3 md:ml-auto md:mr-auto"
+          >
+            <li class="md:inline">
+              <img src="/assets/images/star.webp" alt="Artist name separator" width="4%" class="hidden m-2 md:inline"/>
+              Z-Boys
+            </li>
+            <li class="md:inline">
+              <img src="/assets/images/star.webp" alt="Artist name separator" width="4%" class="hidden m-2 md:inline"/>
+              Allison Cowrey
+              <img src="/assets/images/star.webp" alt="Artist name separator" width="4%" class="hidden m-2 md:inline"/>
+            </li>
           </ul>
 
           <p class="mt-8 italic text-center">

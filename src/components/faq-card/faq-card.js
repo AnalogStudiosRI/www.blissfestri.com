36e4ec90eade51c5687cb5ce7f4affdc1371d758
Owen Buckley <owen@thegreenhouse.io>
2023-05-21 13:35:29 -0400

    FAQ page styling tweaks

--- src/components/faq-card/faq-card.js
+++ src/components/faq-card/faq-card.js
@@ -5,12 +5,14 @@ class FAQCard extends HTMLElement {
     const label = this.getAttribute('label');
 
     this.innerHTML = `
-      <h3 id="${label}" class="text-accent font-primary uppercase text-2xl font-extrabold mt-4 text-left">
-        ${heading}
-      </h3>
-      <p class="text-primary font-primary font-bold text-left">
-        ${content}
-      </p>
+      <div class="mt-16 mb-16">
+        <h3 id="${label}" class="text-accent font-primary uppercase text-2xl font-extrabold mt-4 text-left">
+          ${heading}
+        </h3>
+        <p class="text-primary font-primary font-bold text-left">
+          ${content}
+        </p>
+      </div>
     `;
   }
 }

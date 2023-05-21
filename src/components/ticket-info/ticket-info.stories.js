11c3f7ff038623103919931abb4e32ea66474c4e
Owen Buckley <owen@thegreenhouse.io>
2023-05-21 17:30:29 -0400

    Feature/issue 50 ticket info component (#83)
    * ticket info component and story
    
    * add basic test case for ticket info component
    
    * generic wavy edges styles

---
+++ src/components/ticket-info/ticket-info.stories.js
@@ -0,0 +1,10 @@
+import '../../styles/theme.css';
+import './ticket-info.js';
+
+export default {
+  title: 'Components/Ticket Info'
+};
+
+const Template = () => '<bf-ticket-info></bf-ticket-info>';
+
+export const Primary = Template.bind({});

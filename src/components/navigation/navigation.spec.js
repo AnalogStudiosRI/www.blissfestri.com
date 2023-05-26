247e47f9808b3e827e90fd7544b26c482648f8ee
Owen Buckley <owen@thegreenhouse.io>
2023-05-26 10:18:28 -0400

    add news link to navigation

--- src/components/navigation/navigation.spec.js
+++ src/components/navigation/navigation.spec.js
@@ -27,7 +27,7 @@ describe('Components/Navigation', () => {
     });
 
     it('should have the expected number of links in the nav', () => {
-      expect(links.length).to.equal(4);
+      expect(links.length).to.equal(5);
     });
 
     it('should have the expected links in the correct order', () => {
@@ -38,7 +38,8 @@ describe('Components/Navigation', () => {
       // expect(navLinks[2]).to.equal('/lineup/');
       expect(navLinks[1]).to.equal('/tickets/');
       expect(navLinks[2]).to.equal('/faq/');
-      expect(navLinks[3]).to.equal('/contact/');
+      expect(navLinks[3]).to.equal('/news/');
+      expect(navLinks[4]).to.equal('/contact/');
     });
 
     it('should have the expected link labels in the correct order', () => {
@@ -49,7 +50,8 @@ describe('Components/Navigation', () => {
       // expect(navText[2]).to.equal('Lineup');
       expect(navText[1]).to.equal('Tickets');
       expect(navText[2]).to.equal('FAQ');
-      expect(navText[3]).to.equal('Contact');
+      expect(navText[3]).to.equal('News');
+      expect(navText[4]).to.equal('Contact');
     });
 
     after(async () => {

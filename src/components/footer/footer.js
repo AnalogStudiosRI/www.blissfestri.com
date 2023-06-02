31aa21e91d01e90a64004234af71605cd1a98471
Owen Buckley <owen@thegreenhouse.io>
2023-06-02 10:42:34 -0400

    add privacy link to the footer and update specs

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -49,7 +49,10 @@ export default class Footer extends HTMLElement {
           </a>
         </div>
 
-        <small class="text-white text-xs font-primary">
+        <small
+          class="text-white text-xs font-primary"
+          aria-label="Copyright Notice"
+        >
           Copyright &copy;
 
           <a
@@ -62,6 +65,19 @@ export default class Footer extends HTMLElement {
 
           All Rights Reserved.
         </small>
+
+        <small
+          class="block text-white font-primary"
+          aria-label="Privacy Policy"
+        >
+          <a
+            href="/privacy-policy/"
+            title="Blissfest Privacy Policy" class="underline"
+          >
+            Privacy Policy
+          </a>
+        </small>
+
       </footer>
     `;
   }
--- src/components/footer/footer.spec.js
+++ src/components/footer/footer.spec.js
@@ -20,12 +20,12 @@ describe('Components/Footer', () => {
     it('should have the expected number of links on the page', () => {
       const links = document.querySelectorAll('a');
 
-      expect(links.length).to.equal(4);
+      expect(links.length).to.equal(5);
     });
 
     describe('Social Icon Links', () => {
       it('should have the expected Facebook social link icon', () => {
-        const facebookLink = document.querySelectorAll('a[aria-label="Analog Studios Facebook page"');
+        const facebookLink = document.querySelectorAll('a[aria-label="Analog Studios Facebook page"]');
         const facebookImage = facebookLink[0].querySelectorAll('img');
 
         expect(facebookLink.length).to.equal(1);
@@ -33,7 +33,7 @@ describe('Components/Footer', () => {
       });
 
       it('should have the expected YouTube social link icon', () => {
-        const youtubeLink = document.querySelectorAll('a[aria-label="Analog Studios YouTube page"');
+        const youtubeLink = document.querySelectorAll('a[aria-label="Analog Studios YouTube page"]');
         const youtubeImage = youtubeLink[0].querySelectorAll('img');
 
         expect(youtubeLink.length).to.equal(1);
@@ -41,7 +41,7 @@ describe('Components/Footer', () => {
       });
 
       it('should have the expected Instagram social link icon', () => {
-        const instagramLink = document.querySelectorAll('a[aria-label="Dave Flamand Instagram page"');
+        const instagramLink = document.querySelectorAll('a[aria-label="Dave Flamand Instagram page"]');
         const instagramImage = instagramLink[0].querySelectorAll('img');
 
         expect(instagramLink.length).to.equal(1);
@@ -51,10 +51,19 @@ describe('Components/Footer', () => {
 
     describe('Copyright and Disclaimers', () => {
       it('should have the expected copyright notice', () => {
-        const copyright = document.querySelectorAll('small');
+        const copyright = document.querySelectorAll('[aria-label="Copyright Notice"]');
 
         expect(copyright.length).to.equal(1);
         expect(copyright[0].textContent.replace(/ /g, '').replace(/\n/g, '')).to.equal('Copyright©AnalogStudios.AllRightsReserved.');
+      });
+
+      it('should have the expected privacy policy notice', () => {
+        const privacyContainer = document.querySelectorAll('[aria-label="Privacy Policy"]');
+        const privacyLink = privacyContainer[0].querySelectorAll('a');
+
+        expect(privacyContainer.length).to.equal(1);
+        expect(privacyLink[0].getAttribute('href')).to.equal('/privacy-policy/');
+        expect(privacyLink[0].textContent.replace(/ /g, '').replace(/\n/g, '')).to.equal('PrivacyPolicy');
       });
     });
   });

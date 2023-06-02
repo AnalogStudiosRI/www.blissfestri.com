7b026a638a5f733dded9a54d8a2d7e59a88e58af
Owen Buckley <owen@thegreenhouse.io>
2023-06-02 09:42:29 -0400

    Feature/issue 49 footer component (#91)
    * create footer component
    
    * create footer storybook
    
    * add test cases
    
    * formatting
    
    * integrate footer into the site
    
    * update newsletter specs
    
    * update newsletter specs
    
    * improve iframe responsiveness for tickets page
    
    * improve iframe responsiveness for tickets page
    
    * track known issue with eventbrite iframe overlapping the footer for local development

---
+++ src/components/footer/footer.stories.js
@@ -0,0 +1,10 @@
+import '../../styles/theme.css';
+import './footer.js';
+
+export default {
+  title: 'Components/Footer'
+};
+
+const Template = () => '<bf-footer></bf-footer>';
+
+export const Primary = Template.bind({});
--- src/components/newsletter-signup/newsletter-signup.spec.js
+++ src/components/newsletter-signup/newsletter-signup.spec.js
@@ -22,20 +22,16 @@ describe('Components/Newsletter Signup', () => {
       const heading = document.getElementsByTagName('h2')[0];
       const tagLine = document.getElementsByTagName('p')[0];
       const button = document.getElementsByTagName('button')[0];
-      const copyright = document.getElementsByTagName('small')[0];
-      const lables = document.getElementsByTagName('label');
+      const labels = document.getElementsByTagName('label');
 
       expect(image.alt).to.equal('popped open bottle with music sheet');
       expect(heading.textContent.trim()).to.equal('Stay in Touch!');
       expect(tagLine.textContent.trim()).to.equal(
         'Subscribe to stay up to date with the latest Blissfest news and updates!'
       );
-      expect(lables[0].textContent.trim()).to.equal('Name (optional)');
-      expect(lables[1].textContent.trim()).to.equal('Email Address');
+      expect(labels[0].textContent.trim()).to.equal('Name (optional)');
+      expect(labels[1].textContent.trim()).to.equal('Email Address');
       expect(button.textContent.trim()).to.equal('Sign Me Up');
-      expect(copyright.textContent).to.equal(
-        'Copyright © Analog Studios. All Rights Reserved.'
-      );
     });
   });
 

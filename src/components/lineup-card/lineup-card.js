9745786a7811e7b6d139f1ec2dce786314055a13
Owen Buckley <owen@thegreenhouse.io>
2023-06-04 16:07:17 -0400

    add rotation and main socials attribute props

--- src/components/lineup-card/lineup-card.js
+++ src/components/lineup-card/lineup-card.js
@@ -1,22 +1,69 @@
+// TODO could  make this as its own component, e.g. `<bf-social-icon>` (see footer component)
+function getSocialLinksList(socials) {
+  const socialLinkItems = Object.entries(socials).filter(social => social[1]).map(social => {
+    const [label, link] = social;
+    const formattedTitle = `${name}'s ${label.charAt(0).toUpperCase()}${label.slice(1)} page`;
+
+    return `
+      <a
+        href="${link}"
+        aria-label="${formattedTitle}"
+        title="${name}'s ${formattedTitle} page"
+      >
+        <img
+          src="/assets/images/${label}-icon.png"
+          alt="${formattedTitle}"
+          width="40"
+          height="40"
+          loading="lazy"
+        />
+      </a>
+    `;
+  }).join('');
+
+  return socialLinkItems === ''
+    ? ''
+    : `<ul class="flex gap-x-4 mt-4 mb-4">
+        ${socialLinkItems}
+      </ul>`;
+}
+
 class LineupCard extends HTMLElement {
+  constructor() {
+    super();
+    this.DEFAULT_ROTATION = 'center';
+    this.ROTATION_CLASS_MAPPER = {
+      center: 'rotate-0',
+      left: '-rotate-12',
+      right: 'rotate-12'
+    };
+  }
+
   connectedCallback() {
-    // name
-    // bio
-    // picture
     // link (optional) - website
     // link (optional) - spotify
-    // link (optional) - facebook
-    // link (optional) - youtube
-    // link (optional) - instagram
-    // rotation (optional) - (center|left|right)
     const name = this.getAttribute('name');
     const bio = this.getAttribute('bio');
     const picture = this.getAttribute('picture');
+    const socials = {
+      // website: this.getAttribute('website'),
+      // spotify: this.getAttribute('spotify'),
+      facebook: this.getAttribute('facebook'),
+      youtube: this.getAttribute('youtube'),
+      instagram: this.getAttribute('instagram')
+    };
+    const rotation = this.getAttribute('rotation');
+    const rotationClass = rotation && this.ROTATION_CLASS_MAPPER[rotation]
+      ? this.ROTATION_CLASS_MAPPER[rotation]
+      : this.ROTATION_CLASS_MAPPER[this.DEFAULT_ROTATION];
+    const rotationMargin = rotationClass === this.ROTATION_CLASS_MAPPER[this.DEFAULT_ROTATION]
+      ? 'mt-0'
+      : 'mt-4';
 
     this.innerHTML = `
       <div class="flex flex-row flex-wrap w-full">
         <div class="flex-column flex-1">
-          <div class="text-center">
+          <div class="text-center ${rotationClass} ${rotationMargin}">
             <img
               class="bg-slate-50 pb-4 pl-1 pr-1 pt-1 inline-block w-2/3 drop-shadow-lg lg:pb-16 lg:pl-4 lg:pr-4 lg:pt-4"
               src="${picture}"
@@ -27,6 +74,7 @@ class LineupCard extends HTMLElement {
         <div class="flex-column flex-1 pl-4">
           <h2 class="text-4xl bold font-primary text-primary uppercase">${name}<h2>
           <p class="font-primary">${bio}</p>
+          ${getSocialLinksList(socials)}
         </div>
       </div>
     `;
--- src/components/lineup-card/lineup-card.stories.js
+++ src/components/lineup-card/lineup-card.stories.js
@@ -10,6 +10,10 @@ const Template = () => `
     name="Analog"
     bio="The name of the band is analog. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     picture="https://d34k5cjnk2rcze.cloudfront.net/images/artists/analog.jpg"
+    rotation="right"
+    instagram="https://www.instagram.com/dave.flamand/"
+    facebook="https://www.facebook.com"
+    youtube="https://www.youtube.com"
   >
   </bf-lineup-card>
 `;

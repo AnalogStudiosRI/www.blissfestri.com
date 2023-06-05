5a945f820091dc5a09417745b3e83b2ad22a6cec
Owen Buckley <owen@thegreenhouse.io>
2023-06-04 20:01:06 -0400

    add storybook variants

--- src/components/lineup-card/lineup-card.stories.js
+++ src/components/lineup-card/lineup-card.stories.js
@@ -5,17 +5,74 @@ export default {
   title: 'Components/Lineup Card'
 };
 
-const Template = () => `
-  <bf-lineup-card
-    name="Analog"
-    bio="The name of the band is analog. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-    picture="https://d34k5cjnk2rcze.cloudfront.net/images/artists/analog.jpg"
-    rotation="right"
-    instagram="https://www.instagram.com/dave.flamand/"
-    facebook="https://www.facebook.com"
-    youtube="https://www.youtube.com"
-  >
-  </bf-lineup-card>
-`;
+const Template = ({ props }) => {
+  const { name, bio, picture, rotation, instagram, facebook, youtube } = props;
+  const rotationAttr = rotation ? `rotation="${rotation}"` : '';
+  const instagramAttr = instagram ? `instagram="${instagram}"` : '';
+  const facebookAttr = facebook ? `facebook="${facebook}"` : '';
+  const youtubeAttr = youtube ? `youtube="${youtube}"` : '';
+
+  return `
+    <bf-lineup-card
+      name="${name}"
+      bio="${bio}"
+      picture="${picture}"
+      ${rotationAttr}
+      ${instagramAttr}
+      ${facebookAttr}
+      ${youtubeAttr}
+    >
+    </bf-lineup-card>
+  `;
+};
 
 export const Primary = Template.bind({});
+
+Primary.args = {
+  props: {
+    name: 'Analog',
+    bio: 'The name of the band is analog. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    picture: 'https://d34k5cjnk2rcze.cloudfront.net/images/artists/analog.jpg'
+  }
+};
+
+export const Socials = Template.bind({});
+
+Socials.args = {
+  props: {
+    name: 'Analog',
+    bio: 'The name of the band is analog. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    picture: 'https://d34k5cjnk2rcze.cloudfront.net/images/artists/analog.jpg',
+    instagram: 'https://www.instagram.com/dave.flamand/',
+    facebook: 'https://www.facebook.com',
+    youtube: 'https://www.youtube.com'
+  }
+};
+
+export const RotationRight = Template.bind({});
+
+RotationRight.args = {
+  props: {
+    name: 'Analog',
+    bio: 'The name of the band is analog. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    picture: 'https://d34k5cjnk2rcze.cloudfront.net/images/artists/analog.jpg',
+    rotation: 'right',
+    instagram: 'https://www.instagram.com/dave.flamand/',
+    facebook: 'https://www.facebook.com',
+    youtube: 'https://www.youtube.com'
+  }
+};
+
+export const RotationLeft = Template.bind({});
+
+RotationLeft.args = {
+  props: {
+    name: 'Analog',
+    bio: 'The name of the band is analog. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    picture: 'https://d34k5cjnk2rcze.cloudfront.net/images/artists/analog.jpg',
+    rotation: 'left',
+    instagram: 'https://www.instagram.com/dave.flamand/',
+    facebook: 'https://www.facebook.com',
+    youtube: 'https://www.youtube.com'
+  }
+};

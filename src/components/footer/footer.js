04e442ba6bbc5702ebaaee7067ff877552b621a1
Faraz Khalil <70030525+mughalfrazk@users.noreply.github.com>
2023-06-13 04:19:09 +0500

    enhancement/issue 93 reuse header background image for footer (#98)
    * reuse header tear-strip image for footer
    
    * move transformed text out of background and styled
    
    ---------
    
    Co-authored-by: Faraz Mughal <FMughal@mobilehealth.com>

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,67 +1,68 @@
 export default class Footer extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
-      <footer
-        class="bg-[url('/assets/images/tear-strip-upside-down.webp')] pt-16 pb-4 bg-repeat-x bg-top text-center"
-      >
-        <div class="flex gap-x-4 justify-center mb-4">
-          <a
-            href="https://www.facebook.com/blissfestri/"
-            aria-label="Analog Studios Facebook page"
-            target="_blank"
-            class="ml-2 mr-2"
-          >
-            <img
-              src="/assets/images/facebook-icon.png"
-              alt="Analog Studios Facebook page"
-              width="40"
-              height="40"
-              loading="lazy"
-            />
-          </a>
-          <a
-            href="https://www.youtube.com/channel/UCxs5mxoDpmmR0hRbwsxU7Sg/"
-            aria-label="Analog Studios YouTube page"
-            target="_blank"
-            class="ml-2 mr-2"
-          >
-            <img
-              src="/assets/images/youtube-icon.png"
-              alt="Analog Studios YouTube page"
-              width="40"
-              height="40"
-              loading="lazy"
-            />
-          </a>
-          <a
-            href="https://www.instagram.com/dave.flamand/"
-            aria-label="Dave Flamand Instagram page"
-            target="_blank"
-            class="ml-2 mr-2"
-          >
-            <img
-              src="/assets/images/instagram-icon.png"
-              alt="Dave Flamand Instagram page"
-              width="40"
-              height="40"
-              loading="lazy"
-            />
-          </a>
+      <footer class="text-center">
+        <div class="bg-[url('/assets/images/tear-strip.webp')] transform rotate-180 h-20 bg-no-repeat bg-top">
         </div>
+        <div class="bg-primary pb-4">
+          <div class="flex gap-x-4 justify-center mb-4">
+            <a
+              href="https://www.facebook.com/blissfestri/"
+              aria-label="Analog Studios Facebook page"
+              target="_blank"
+              class="ml-2 mr-2"
+            >
+              <img
+                src="/assets/images/facebook-icon.png"
+                alt="Analog Studios Facebook page"
+                width="40"
+                height="40"
+                loading="lazy"
+              />
+            </a>
+            <a
+              href="https://www.youtube.com/channel/UCxs5mxoDpmmR0hRbwsxU7Sg/"
+              aria-label="Analog Studios YouTube page"
+              target="_blank"
+              class="ml-2 mr-2"
+            >
+              <img
+                src="/assets/images/youtube-icon.png"
+                alt="Analog Studios YouTube page"
+                width="40"
+                height="40"
+                loading="lazy"
+              />
+            </a>
+            <a
+              href="https://www.instagram.com/dave.flamand/"
+              aria-label="Dave Flamand Instagram page"
+              target="_blank"
+              class="ml-2 mr-2"
+            >
+              <img
+                src="/assets/images/instagram-icon.png"
+                alt="Dave Flamand Instagram page"
+                width="40"
+                height="40"
+                loading="lazy"
+              />
+            </a>
+          </div>
 
-        <small
-          class="text-white text-xs font-primary"
-          aria-label="Copyright Notice"
-        >
-          Copyright &copy;
-
-          <a
-            href="https://www.analogstudios.net"
-            title="Analog Studios website"
-            aria-label="Analog Studios website"
+          <small
+            class="text-white text-xs font-primary"
+            aria-label="Copyright Notice"
           >
-            Analog Studios
-          </a>.
+            Copyright &copy;
+
+            <a
+              href="https://www.analogstudios.net"
+              title="Analog Studios website"
+              aria-label="Analog Studios website"
+            >
+              Analog Studios
+            </a>.
 
           All Rights Reserved.
         </small>
@@ -83,7 +84,6 @@ export default class Footer extends HTMLElement {
           >
             Terms of Use</a>
         </small>
-
       </footer>
     `;
   }

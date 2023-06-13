abd3112bd9a2be2264ab0c7115cbfe0c02df369b
Owen Buckley <owen@thegreenhouse.io>
2023-06-13 08:39:12 -0400

    responsiveness across all breakpoints

--- src/pages/lineup.js
+++ src/pages/lineup.js
@@ -25,9 +25,9 @@ export default class LineupPage extends HTMLElement {
       const rotation = idx % 2 === 0 ? 'left' : 'right';
 
       return `
-        <div class="mt-4 mb-4">
-          <span class="inline-block align-top text-center mb-8 mr-8">
-            <span class="bg-primary text-3xl lg:text-5xl font-secondary text-secondary p-2">${formatStartTime(startTime)}</span>
+        <div class="mt-4 mb-8 md:pl-24">
+          <span class="inline-block bg-primary align-top text-center mb-8 mr-8 lg:w-1/5">
+            <span class="font-secondary text-secondary p-2 inline-block text-4xl text-center">${formatStartTime(startTime)}</span>
           </span>
 
           <bf-lineup-card

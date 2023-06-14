f1fa72b22fd48408be338290de07188e76dca298
Owen Buckley <owen@thegreenhouse.io>
2023-06-14 08:10:36 -0400

    self hosting lineup images

--- src/services/lineup-service.js
+++ src/services/lineup-service.js
@@ -2,7 +2,7 @@
 const LINEUP = [{
   name: 'Jabbawaukee',
   bio: 'Jabbawaukee is a Southern New England based quartet playing psychedelic funk and jam rock!',
-  picture: 'https://scontent-bos5-1.xx.fbcdn.net/v/t39.30808-6/348597984_2443540075804741_862726556603536021_n.jpg?stp=cp6_dst-jpg&_nc_cat=111&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=zSTp6xZ-BnIAX8jemax&_nc_ht=scontent-bos5-1.xx&oh=00_AfCVReR0KIONrPdt_L745KpIR6tZpIJ4Yicl2ajHGlTWAg&oe=6488471D',
+  picture: '/assets/images/lineup/jabbawaukee.webp',
   facebook: 'https://www.facebook.com/JABBAWAUKEE/',
   instagram: 'https://www.instagram.com/jabbawaukee_band/',
   website: 'https://www.jabbawaukee.com',
@@ -12,8 +12,8 @@ const LINEUP = [{
   startTime: 1690682400000 // 10pm
 }, {
   name: 'Analog',
-  bio: 'Analog is a power duo consisting of guitar and drums.  Strong rock beats, smooth vocals, and loud guitar meld together to create Analog\'s unique sound.',
-  picture: 'https://d34k5cjnk2rcze.cloudfront.net/images/artists/analog.jpg',
+  bio: 'Analog is a power trio consisting of guitar, bass and drums.  Strong rock beats, smooth vocals, and loud guitar meld together to create Analog\'s unique sound.',
+  picture: '/assets/images/lineup/analog.webp',
   facebook: '',
   instagram: 'https://www.instagram.com/analog.newport/',
   website: 'https://www.analogstudios.net/artists/1',
@@ -24,7 +24,7 @@ const LINEUP = [{
 }, {
   name: 'Bill Bartholomew',
   bio: 'Singer-songwriter + podcaster.',
-  picture: 'https://d1fdloi71mui9q.cloudfront.net/ukAnZ2ARmCZdMrfaBHIc_5kjKKBWg1aHgO62n',
+  picture: '/assets/images/lineup/bill-bartholomew.webp',
   facebook: 'https://www.facebook.com/billbartholomewmusic/',
   instagram: 'https://www.instagram.com/billbartholomew/',
   website: 'https://www.analogstudios.net/artists/1',
@@ -35,7 +35,7 @@ const LINEUP = [{
 }, {
   name: 'The Z-Boys',
   bio: 'The Z-Boys is a power trio playing surf rock, soul and funk for your listening and dancing pleasure.',
-  picture: 'https://scontent-bos5-1.xx.fbcdn.net/v/t1.6435-9/168369833_135222521916361_7088195225441111800_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=YwSCwZW-UDQAX-n6IjW&_nc_ht=scontent-bos5-1.xx&oh=00_AfDSa_gTO8rVRa5twMZNmnEJ5Ps9-Xqet6fz-EvebEpZlw&oe=64A9E90A',
+  picture: '/assets/images/lineup/the-z-boys.webp',
   facebook: 'https://www.facebook.com/zboystonight/',
   instagram: 'https://www.instagram.com/zboystonight/',
   website: 'https://zboystonight.com',
@@ -46,7 +46,7 @@ const LINEUP = [{
 }, {
   name: 'Allysen Callery',
   bio: 'Allysen Callery is a self taught singer songwriter from Bristol, Rhode Island, whose delicate, mesmerizing sound has been compared to classic British folk artists like Sandy Denny and Nick Drake.',
-  picture: 'https://scontent-bos5-1.xx.fbcdn.net/v/t39.30808-6/338289986_1903874899972494_8661821939696629366_n.jpg?stp=cp6_dst-jpg&_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=H9f9W4PeQTUAX-RXoM9&_nc_ht=scontent-bos5-1.xx&oh=00_AfDP3YqH0cX9IXL74YC_woL024vJO6IqMVCNpqb9B-wIRg&oe=64874727',
+  picture: '/assets/images/lineup/allysen-callery.webp',
   facebook: 'https://www.facebook.com/allysencallerymusic/',
   instagram: 'https://www.instagram.com/allysencallery/',
   website: 'https://allysencallerymusic.com',
@@ -57,7 +57,7 @@ const LINEUP = [{
 }, {
   name: 'Nate Farrar',
   bio: '',
-  picture: 'https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg',
+  picture: '/assets/images/lineup/nate-farrar.webp',
   facebook: '',
   instagram: 'https://www.instagram.com/splinter_hippie',
   website: '',

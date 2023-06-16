59f0ef8a0f6b3c21d791c914555d2a167ee2e57e
Owen Buckley <owen@thegreenhouse.io>
2023-06-16 08:36:13 -0400

    add jesse the tree to the lineup

--- src/services/lineup-service.js
+++ src/services/lineup-service.js
@@ -9,10 +9,10 @@ const LINEUP = [{
   spotify: 'https://open.spotify.com/artist/1CBlzYSiHvEO86x2UO85u4',
   youtube: 'https://www.youtube.com/channel/UCjNsiyNaoh7-Xp6rtCgq4BA',
   apple: 'https://music.apple.com/us/artist/jabbawaukee/1593104536',
-  startTime: 1690682400000 // 10pm
+  startTime: 1690683300000 // 10:15pm
 }, {
   name: 'Analog',
-  bio: 'Analog is a power trio consisting of guitar, bass and drums.  Strong rock beats, smooth vocals, and loud guitar meld together to create Analog\'s unique sound.',
+  bio: 'Analog is a Newport, RI based power trio consisting of guitar, bass and drums.  Strong rock beats, smooth vocals, and loud guitar meld together to create Analog\'s unique sound.',
   picture: '/assets/images/lineup/analog.webp',
   facebook: '',
   instagram: 'https://www.instagram.com/analog.newport/',
@@ -20,7 +20,7 @@ const LINEUP = [{
   spotify: '',
   youtube: 'https://www.youtube.com/channel/UCxs5mxoDpmmR0hRbwsxU7Sg',
   apple: '',
-  startTime: 1690678800000 // 9pm
+  startTime: 1690680300000 // 9:25pm
 }, {
   name: 'Bill Bartholomew',
   bio: 'Bill Bartholomew is an indie artist and podcaster. His music has been featured on VICE, Showtime, MTV and many other platforms.',
@@ -31,7 +31,7 @@ const LINEUP = [{
   spotify: 'https://open.spotify.com/artist/2lTJtrORdzzAfWQulZrkdD',
   youtube: 'https://www.youtube.com/channel/UCEWfrWiFc_6FdOjkdr21wBw',
   apple: 'https://music.apple.com/us/artist/bill-bartholomew/279548573',
-  startTime: 1690675200000 // 8pm
+  startTime: 1690677300000 // 8:35pm
 }, {
   name: 'The Z-Boys',
   bio: 'The Z-Boys is a power trio playing surf rock, soul and funk for your listening and dancing pleasure.',
@@ -42,6 +42,17 @@ const LINEUP = [{
   spotify: 'https://open.spotify.com/artist/69UMUUtRn8kOqOkF6EqEFl',
   youtube: 'https://www.youtube.com/zboystonight',
   apple: 'https://music.apple.com/us/artist/the-z-boys/1192510207',
+  startTime: 1690674300000 // 7:45pm
+}, {
+  name: 'jesse the Tree',
+  bio: ' Rhode Island emcee and beatsmith jesse the Tree brings influences ranging from a more traditionally organic and melodic corner of the music world to sounds closer to his own output of hip hop and contemporary underground.   ',
+  picture: '/assets/images/lineup/jesse-the-tree.webp',
+  facebook: 'https://www.facebook.com/jessethetree/',
+  instagram: 'https://www.instagram.com/jessethetree/',
+  website: 'https://www.strangefamousrecords.com/artists/jesse-the-tree/',
+  spotify: 'https://open.spotify.com/artist/4mp2W09kX6MXfCl4R9uYae',
+  youtube: 'https://www.youtube.com/channel/UCazcvHOtjQ7FFyNE3iJuiUA',
+  apple: 'https://music.apple.com/us/artist/jesse-the-tree/1310806252',
   startTime: 1690671600000 // 7pm
 }, {
   name: 'Allysen Callery',

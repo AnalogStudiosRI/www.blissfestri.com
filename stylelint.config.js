b6e146b7f1487225954d632f32886895db1465f5
Owen Buckley <owenbuckley13@gmail.com>
2024-01-04 12:51:04 -0500

    fix linting

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,13 +2,13 @@
 export default {
   // plugins: ['stylelint-a11y'],
   extends: [
-    'stylelint-config-standard',
+    'stylelint-config-standard'
     // 'stylelint-a11y/recommended'
   ],
   rules: {
     'at-rule-no-unknown': [true, { ignoreAtRules: ['tailwind'] }],
     'no-empty-source': null,
     'declaration-empty-line-before': null,
-    'value-keyword-case': null,
+    'value-keyword-case': null
   }
 };
